5ea4e8b5fde9cca1eb26ad5ccd8291dbfe34500c
madeleinewoodbury <randine25@gmail.com>
2020-05-03 15:42:05 -0500

    Login and styling

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === '' || password === '') {
+      alert('Fill in all fields');
+    } else {
+      console.log('Success');
+    }
+  };
+  return (
+    <div className="container">
+      <div className="form-container">
+        <h1 className="large">Log In</h1>
+        <p className="lead">
+          <i className="fas fa-user"></i> Sign in with your admin credentials
+        </p>
+        <form className="form">
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="E-postadresse"
+              name="email"
+              value={email}
+              onChange={(e) => handleChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Passord"
+              name="password"
+              value={password}
+              onChange={(e) => handleChange(e)}
+              minLength={6}
+              required
+            />
+          </div>
+          <input
+            type="submit"
+            className="btn btn-secondary"
+            value="Logg Inn"
+            onClick={(e) => handleSubmit(e)}
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar bg-primary">
+    <nav className="navbar">
       <Link to="/">ESC Admin</Link>
       <ul>
         <li>
